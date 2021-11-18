8fae1b449dd96b118ec8b9c8715c5c084ec142bb
ephayeabe <ephremyeabe@gmail.com>
2021-11-18 01:12:11 -0800

    update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    size=len(sys.argv)
+    size = len(sys.argv)
     if size-1 == 1:
-        print("{:d} argument".format(size - 1),end="")
+        print("{:d} argument".format(size - 1), end="")
     else:
-        print("{:d} arguments".format(size - 1),end="")
+        print("{:d} arguments".format(size - 1), end="")
     if size - 1 == 0:
         print(".")
     else:
         print(":")
     for i in range(1, size):
         print("{:d}: {:s}".format(i, sys.argv[i]))
-    
