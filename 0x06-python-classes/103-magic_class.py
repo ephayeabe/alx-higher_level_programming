a8095072fe2540aee1f39bb10f7ef5f3cf6675a2
ephayeabe <57351825+ephayeabe@users.noreply.github.com>
2021-12-07 15:15:31 +0300

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Class docstring"""
 import math
 
 
 class MagicClass:
     def __init__(self, radius=0):
-        """Init docstring
-        """
-        self.__radius = 0
+        self.__radius = radius
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError('radius must be a number')
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Area docstring"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Circumference docstring"""
         return 2 * math.pi * self.__radius
+    
