ab188d2ec005c83896b16b98268bcb7df1587873
ephayeabe <ephremyeabe@gmail.com>
2022-03-01 03:28:31 -0800

    fix semistandard for 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
-let newList = list.map((x, index) => x * index);
+const newList = list.map((x, index) => x * index);
 console.log(newList);
