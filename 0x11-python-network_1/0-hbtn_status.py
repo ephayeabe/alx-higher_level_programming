aa27490221e11a8edbda1309f518ca22039bb586
ephayeabe <ephremyeabe@gmail.com>
2022-03-18 15:34:23 +0300

    upload task 0:A Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status.
+it Fetches the website content
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
